74d3f04f899b9b43868c67c9a5d73306688529f8
Lee <58116162+VirtDev337@users.noreply.github.com>
2023-05-14 15:12:00 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -25,70 +25,73 @@ else
     sudo apt install -y software-properties-common apt-transport-https wget git curl > /dev/null
 fi
 
-while getopts "?acCikr" option
-do
-    case $option in
-        a)
-            if ( apt list --installed | grep android-studio ) > /dev/null
-            then
-                echo "Android Studio is already installed."
-            else
-                echo "Installing Android Studio..."
-                sudo dpkg --add-architecture i386
-                sudo apt install -y openjdk-14-jdk libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 > /dev/null
+if [ $# > 0 ]
+then
+  while getopts "?acCikr" option
+  do
+      case $option in
+          a)
+              if ( apt list --installed | grep android-studio ) > /dev/null
+              then
+                  echo "Android Studio is already installed."
+              else
+                  echo "Installing Android Studio..."
+                  sudo dpkg --add-architecture i386
+                  sudo apt install -y openjdk-14-jdk libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 > /dev/null
 
-                sudo add-apt-repository -y ppa:maarten-fonville/android-studio > /dev/null
-                
-                sudo apt install -y android-studio
-            fi
-        ;;
-        c)
-            if ( apt list --installed | grep code/stable ) > /dev/null
-            then
-                echo "VS Code is already installed."
-            else
-                echo "Installing VS Code..."
-                sudo apt install -y code
-            fi
-        ;;
-        C)
-            if ( apt list --installed | grep codium ) > /dev/null
-            then
-                echo "VS Codium is already installed."
-            else
-                echo "Installing VS Codium..."
-                curl -fSsL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscodium.gpg > /dev/null
-                
-                echo deb [signed-by=/usr/share/keyrings/vscodium.gpg] https://download.vscodium.com/debs vscodium main | sudo tee /etc/apt/sources.list.d/vscodium.list > /dev/null
-                
-                sudo apt update > /dev/null
-                sudo apt install -y codium
-            fi
-        ;;
-        i)
-            if ( apt list --installed | grep intellij-idea-* ) > /dev/null
-            then
-                echo "IDEA is already installed."
-            else
-                echo "Installing IntelliJ IDEA Community Edition..."
-                sudo add-apt-repository -y ppa:mmk2410/intellij-idea > /dev/null
-                sudo apt update > /dev/null
-                sudo apt install -y intellij-idea-community
-            fi
-        ;;
-        k)
-            keep=true
-        ;;
-        r)
-            run=true
-        ;;
-        ?) 
-            echo $usage
-            echo ""
-            exit
-        ;;
-    esac
-done
+                  sudo add-apt-repository -y ppa:maarten-fonville/android-studio > /dev/null
+
+                  sudo apt install -y android-studio
+              fi
+          ;;
+          c)
+              if ( apt list --installed | grep code/stable ) > /dev/null
+              then
+                  echo "VS Code is already installed."
+              else
+                  echo "Installing VS Code..."
+                  sudo apt install -y code
+              fi
+          ;;
+          C)
+              if ( apt list --installed | grep codium ) > /dev/null
+              then
+                  echo "VS Codium is already installed."
+              else
+                  echo "Installing VS Codium..."
+                  curl -fSsL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscodium.gpg > /dev/null
+
+                  echo deb [signed-by=/usr/share/keyrings/vscodium.gpg] https://download.vscodium.com/debs vscodium main | sudo tee /etc/apt/sources.list.d/vscodium.list > /dev/null
+
+                  sudo apt update > /dev/null
+                  sudo apt install -y codium
+              fi
+          ;;
+          i)
+              if ( apt list --installed | grep intellij-idea-* ) > /dev/null
+              then
+                  echo "IDEA is already installed."
+              else
+                  echo "Installing IntelliJ IDEA Community Edition..."
+                  sudo add-apt-repository -y ppa:mmk2410/intellij-idea > /dev/null
+                  sudo apt update > /dev/null
+                  sudo apt install -y intellij-idea-community
+              fi
+          ;;
+          k)
+              keep=true
+          ;;
+          r)
+              run=true
+          ;;
+          ?) 
+              echo $usage
+              echo ""
+              exit
+          ;;
+      esac
+  done
+fi
 
 if ( git --version )
 then
